44a1438940a0ee4591ac12574c002715a282906e
antoinelutti <antoinelutti@chuv.ch>
2021-04-06 14:07:39 +0200

    minor changes in the comments

--- hmri_quiqi_build.m
+++ hmri_quiqi_build.m
@@ -100,22 +100,22 @@ end
 
 if isfield(SPM.xVi,'Vi')
     % check if it is a group comparison
-    nVi=size(SPM.xVi.Vi,2);%AL change
+    nVi=size(SPM.xVi.Vi,2);
     for v=1:nVi
         len(v)=length(find(diag(SPM.xVi.Vi{v})~=0));
     end
-    if length(unique(len))>1  % This is a group comparison with sizes of the group different
+    if length(unique(len))>1  % This is a group comparison with different sizes for both groups 
         [Uni inda indc]=unique(len);
         GroupIndx={};
         for ctr=1:length(unique(len))
             GroupIndx{ctr}=find(diag(SPM.xVi.Vi{inda(ctr)})~=0);
         end
     else
-        if unique(len)==size(SPM.xVi.Vi{1},1)/2 % This is a group comparison with same size of the groups
+        if unique(len)==size(SPM.xVi.Vi{1},1)/2 % This is a comparison of two groups with the same size 
             GroupIndx{1}=find(diag(SPM.xVi.Vi{1})~=0);
             GroupIndx{2}=find(diag(SPM.xVi.Vi{end})~=0);
         else % This is not a group comparison 
-        GroupIndx{1}=find(diag(SPM.xVi.V)~=0);
+            GroupIndx{1}=find(diag(SPM.xVi.V)~=0);
         end
     end
 else % this is not a group comparison 
